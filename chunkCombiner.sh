7026061016c804560155c0501f8bbc66317d1dfb
mpdcampbell <47434940+mpdcampbell@users.noreply.github.com>
2022-11-18 22:21:51 +0000

    Cat and get file extension

--- chunkCombiner.sh
+++ chunkCombiner.sh
@@ -12,3 +12,25 @@ rm $dirListPath.tmp;
 
 head -5 $dirListPath.txt
 wc -l < $dirListPath.txt
+
+if [ ! -d $outputDir ]; then
+    mkdir $outputDir;
+fi
+
+#Dictionary of mime-type and extension
+
+extByType[jpeg]=.jpeg
+extByType[png]=.png
+extByType[quicktime]=.qt
+extByType[mp4]=.mp4
+
+i=0;
+while read -r line; do
+    cd "$line" ; cat ~+/* >> $line/output.tmp;
+    mimetype=$(file -b --mime-type output.tmp | cut -d "/" -f 2);
+    extension=${extByType[$mimetype]};
+    mv output.tmp $outputDir/file_$i$extension;
+    ((i=i+1));
+done <$dirListPath.txt
+
+rm $dirListPath.txt
