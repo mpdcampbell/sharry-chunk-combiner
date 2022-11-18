e1c413b36a699fe794a55394dc5bd13dd362914a
mpdcampbell <47434940+mpdcampbell@users.noreply.github.com>
2022-11-18 22:48:36 +0000

    Add padded zeroes to filename

--- chunkCombiner.sh
+++ chunkCombiner.sh
@@ -24,12 +24,17 @@ extByType[png]=.png
 extByType[quicktime]=.qt
 extByType[mp4]=.mp4
 
-i=0;
+#Get padded zero count for filename
+fileCount=$(wc -l < $dirListPath.txt);
+digitCount=${#fileCount};
+i=1;
+
 while read -r line; do
     cd "$line" ; cat ~+/* >> $line/output.tmp;
     mimetype=$(file -b --mime-type output.tmp | cut -d "/" -f 2);
     extension=${extByType[$mimetype]};
-    mv output.tmp $outputDir/file_$i$extension;
+    paddedInt=$(printf "%0${digitCount}d" ${i});
+    mv output.tmp $outputDir/file_$paddedInt$extension;
     ((i=i+1));
 done <$dirListPath.txt
 
