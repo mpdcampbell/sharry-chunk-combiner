74c8686e0d95eca487a8fa511c0b786bd6e936af
mpdcampbell <47434940+mpdcampbell@users.noreply.github.com>
2022-11-21 01:54:58 +0000

    Added flags for database and output directory input

--- chunkCombiner.sh
+++ chunkCombiner.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
 
-#File Paths
-databaseDir= #ENTER PATH TO DATABASE DIRECTORY eg /home/user/sharrydb/
-outputDir= #ENTER PATH TO DIRECTORY TO OUTPUT RECOMBINED FILES
-dirListPath=$outputDir/dirList
+function usage {
+    echo "Usage: $(basename $0) [-d -o]"
+    echo "  Mandatory Flags."
+    echo "    -d    Path to Sharry filesystem database directory."
+    echo "    -o    Path to directory to output rebuilt files."
+#    echo "  Optional Flag."
+#    echo "    -e    File type extensions you want to rebuild."     
+    exit 1
+}
 
-#Check variables were updated
-if [ -z "$databaseDir" ]; then
-  echo "Error: The databaseDir variable is empty, exiting script."
-  exit 1
-elif [ -z "$outputDir" ]; then
-  echo "Error: The outputDir variable is empty, exiting script."
-  exit 1
+if (($# == 0)); then
+    echo "Error: Mandatory Flags missing.";
+    usage;
+fi
+
+optstring=":d:o:";
+
+while getopts ${optstring} arg; do
+    case "${arg}" in
+        d) databaseDir=${OPTARG} ;;
+        o) outputDir=${OPTARG} ;;
+#	e) echo "Not yet implemented." ;;
+        h) usage ;;
+        :) echo "Error: Missing option argument for -${OPTARG}."
+           usage ;;
+        ?) echo "Error: Invalid option: - ${OPTARG}."
+           usage ;;
+    esac
+done
+
+if [ ! -d $databaseDir ]; then
+    echo "Error: Invalid database directory.";
+    usage;
 fi
 
 if [ ! -d $outputDir ]; then
-    mkdir $outputDir;
+    echo "Output directory $outputDir not found.";
+    echo "Create directory $outputDir and continue? [Y/N]";
+    read input;
+    if [[ $input == "Y" || $input == "y" ]]; then
+        mkdir $outputDir;
+	echo "Created directory $outputDir.";
+    elif [[ $input == "N" || $input == "n" ]]; then
+	echo "Exiting.";
+	exit 0;
+    else
+	echo "Invalid [Y/N] input.";
+	echo "Exiting.";
+	exit 1;
+    fi
 fi
 
 echo "Generating list of data directories.";
+dirListPath=$outputDir/dirList
 find $databaseDir -type f -exec dirname {} >> $dirListPath.tmp +;
 awk '!a[$0]++' $dirListPath.tmp >> $dirListPath.txt;
 rm $dirListPath.tmp;
@@ -28,7 +63,7 @@ declare -A extByType;
 while read -r line; do
     mimeType=$(echo $line | cut -d "|" -f 1);
     ext=$(echo $line | cut -d "|" -f 2);
-    extByType[$mimetype]="$ext";
+    extByType[$mimeType]="$ext";
 done < extByMimeType.txt
 
 #Get padded zero count for filename
